be0d01429aee48eda17868dd834e7539b8ed0d3c
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2019-08-18 23:20:20 +0200

    Set bash to interactive mode

--- alpine/python/first_install_bash_alpine.sh
+++ alpine/python/first_install_bash_alpine.sh
@@ -16,7 +16,7 @@ apk add --no-cache bash
 COMPANION_FILE='./install_python_alpine_root.sh'
 
 if test -f "$COMPANION_FILE"; then
-    /bin/bash ./install_python_alpine_root.sh
+    /bin/bash -i ./install_python_alpine_root.sh
 else
-    wget -qo- https://raw.githubusercontent.com/drconopoima/sysadmin_devops_scripts/master/alpine/python/install_python_alpine_root.sh | /bin/bash --init-file
+    wget -qo- https://raw.githubusercontent.com/drconopoima/sysadmin_devops_scripts/master/alpine/python/install_python_alpine_root.sh | /bin/bash -i
 fi
